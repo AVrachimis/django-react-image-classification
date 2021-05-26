aa0a5e79f77cb7db2ba37eabdfaa605c3c2f15d4
Andreas Vrachimis <andreas.vr@hotmail.com>
2021-05-26 19:12:47 +0300

    Create test buttons

--- react-frontend/posts/src/App.js
+++ react-frontend/posts/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import { Button, Card } from 'react-bootstrap';
 
 class App extends Component {
   state = {
@@ -11,6 +11,14 @@ class App extends Component {
     return (
       <div className="App">
         <h1>{this.state.text}</h1>
+
+        {this.state.text ?
+          <Button variant='primary' size='lg'>Test Button</Button>
+          :
+          <Button variant='danger' size='lg'>Test Button 2</Button>
+        }
+
+        <Card>Hi there</Card>
       </div>
     );
   }
