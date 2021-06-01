e4bc7a1d4af3184c89ca6f2b3e5bfc5f46b393bf
Andreas Vrachimis <andreas.vr@hotmail.com>
2021-06-01 20:06:37 +0300

    add load more images button

--- react-frontend/image-front/src/components/ImageList/Image.js
+++ react-frontend/image-front/src/components/ImageList/Image.js
@@ -3,14 +3,12 @@ import { Card, Button } from 'react-bootstrap';
 
 const Image = (props) => {
     return (
-        <Card style={{ width: '18rem' }} className="mx-auto mb-2">
+        <Card style={{ width: '30rem' }} className="mx-auto mb-2">
             <Card.Img variant="top" src={props.pict} />
             <Card.Body>
-
                 <Card.Title>
                     Classified as: {props.name}
                 </Card.Title>
-
             </Card.Body>
         </Card>
     );
--- react-frontend/image-front/src/components/ImageList/ImageList.js
+++ react-frontend/image-front/src/components/ImageList/ImageList.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Image from './Image';
-
+import { Button } from 'react-bootstrap'
 class ImageList extends Component {
     state = {
         images: [],
+        visible: 2,
     }
 
     componentDidMount() {
@@ -23,14 +24,22 @@ class ImageList extends Component {
 
 
     }
+
+    handleVisible = () => {
+        const visible = this.state.visible
+        const new_visible = visible + 2
+        this.setState({ visible: new_visible })
+    }
+
     render() {
-        const images = this.state.images.map(img => {
+        const images = this.state.images.slice(0, this.state.visible).map(img => {
             return <Image key={img.id} pict={img.picture} name={img.classified} />
         })
         return (
             <div>
                 <h1> ImageList</h1 >
                 {images}
+                <Button variant='primary' size='lg' onClick={this.handleVisible}>Load more</Button>
             </div>
         );
     }
